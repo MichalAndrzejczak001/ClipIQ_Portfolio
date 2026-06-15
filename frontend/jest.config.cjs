d5cd090d7734ad626989c21e7e6c5e547cfd1077
Michal Andrzejczak <michandrzejczakofficial@gmail.com>
2026-06-16 01:27:04 +0200

    fix(frontend): correct setupFilesAfterEnv typo and add esModuleInterop for ts-jest

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,12 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterFramework: ['<rootDir>/src/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': '<rootDir>/src/__mocks__/styleMock.cjs',
   },
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: { jsx: 'react-jsx', noUnusedLocals: false, noUnusedParameters: false } }],
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: { jsx: 'react-jsx', esModuleInterop: true, noUnusedLocals: false, noUnusedParameters: false } }],
   },
   testMatch: ['**/__tests__/**/*.{ts,tsx}', '**/*.test.{ts,tsx}'],
 }
